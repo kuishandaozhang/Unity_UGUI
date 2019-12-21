2ab462c77dcf0f6cb29e550433c33cb09a3f1a94
freemandoubt <freemandoubt@example.com>
2019-12-21 12:22:04 +0800

    鼠标拖拽UI跟随
    1，Drag事件接口
    2，鼠标位置
    3，鼠标位置与UI位置换算

---
+++ EventInterface/Example/DragUIFollowMouse/Assets/Scripts/DragCtrl.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragCtrl : MonoBehaviour, IDragHandler
+{
+    private RectTransform m_RectTran;
+
+    void Start()
+    {
+        m_RectTran = GetComponent<RectTransform>();
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Vector3 posOffset = new Vector3(Screen.width / 2, Screen.height / 2, 0);     //计算偏移量
+        m_RectTran.anchoredPosition = Input.mousePosition - posOffset;
+    }
+}
