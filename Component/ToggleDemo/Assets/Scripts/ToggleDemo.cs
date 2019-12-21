03410998eb695aade7f842f24fd49c8981360585
kuishandaozhang <kuishandaozhang@example.com>
2019-12-21 12:40:07 +0800

    Toggle组件
    1，组件介绍
    2，脚本控制

---
+++ Component/ToggleDemo/Assets/Scripts/ToggleDemo.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// Toggle复选框Demo
+/// </summary>
+public class ToggleDemo : MonoBehaviour
+{
+    private Toggle m_Toggle;    //Toggle组件
+
+    private GameObject panel;   //面板
+
+    void Start()
+    {
+        m_Toggle = GetComponent<Toggle>();                              //获取组件
+
+        panel = GameObject.Find("Canvas/Panel");                        //获取面板物体
+
+        m_Toggle.onValueChanged.AddListener(OnToggleValueChanged);      //监听Toggle变化，并将信息传到OnToggleValueChanged()方法中
+        m_Toggle.onValueChanged.AddListener(OnToggleValueChanged_2);
+    }
+
+    /// <summary>
+    /// 打印Toggle状态
+    /// </summary>
+    /// <param name="isOn"></param>
+    private void OnToggleValueChanged(bool isOn)
+    {
+        Debug.Log("当前状态：" + isOn);
+    }
+
+    /// <summary>
+    /// 显示或隐藏面板
+    /// </summary>
+    /// <param name="isOn">显示与否</param>
+    private void OnToggleValueChanged_2(bool isOn)
+    {
+        panel.SetActive(isOn);
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Component/ToggleDemo/Assets/Scripts/ToggleManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// Toggle组件管理器Demo
+/// </summary>
+public class ToggleManager : MonoBehaviour
+{
+    private Toggle toggle;      //Toggle组件
+
+    void Start()
+    {
+        toggle = GameObject.Find("Canvas/Toggle").GetComponent<Toggle>();       //获取Toggle组件
+
+        Debug.Log("ToggleManager: " + toggle.name);
+    }
+
+    void Update()
+    {
+        
+    }
+}
