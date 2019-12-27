19a7f297e439b4cc3fb0f14ce67d43bca976722c
kuishandaozhang <kuishandaozhang@example.com>
2019-12-28 01:14:07 +0800

    UIEffects组件
    1，Outline组件；
    2，Shadow组件；

---
+++ Component/UIEffectsDemo/Assets/Scripts/UIEffectDemo.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIEffectDemo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Shadow shadow;
+        Outline outline;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
