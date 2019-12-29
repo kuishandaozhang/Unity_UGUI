d25dce23da0ef085f745c8f098ab0b7e010d108c
kuishandaozhang <kuishandaozhang@example.com>
2019-12-30 02:45:11 +0800

    IPointerEnterHandler接口与IPointerExitHandler接口
    1，IPointerEnterHandler接口；
    2，IPointerExitHandler接口；
    3，Button中Click的实现原理；
    4，Enter与Exit修改颜色与文本；

---
+++ EventInterface/Event/IPointerEnterHandlerAndIPointerExitHandlerDemo/Assets/Scripts/EnterAndExitDemo.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;     //UI接口在该命名空间中
+
+public class EnterAndExitDemo : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    private Image image;
+    private Text text;
+
+    void Start()
+    {
+        image = GetComponent<Image>();
+        text = transform.Find("Text").GetComponent<Text>();
+    }
+
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        Debug.Log("鼠标进入！");
+        image.color = Color.red;    //红色
+        text.text = "鼠标进入";
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        Debug.Log("鼠标移出！！");
+        image.color = Color.green;  //绿色
+        text.text = "鼠标移出";
+    }
+}
