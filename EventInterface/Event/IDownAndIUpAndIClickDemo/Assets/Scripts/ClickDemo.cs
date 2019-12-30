5cf64bb3505588586e50d512c618d11cc17a62db
kuishandaozhang <kuishandaozhang@example.com>
2019-12-31 02:27:46 +0800

    UI事件接口：Down，Up，Click
    1，IPointerDonwHandler接口；
    2，IPointerUpHandler接口；
    3，IPointerClickHandler接口；
    4，动态生成空物体；
    5，动态添加组件；

---
+++ EventInterface/Event/IDownAndIUpAndIClickDemo/Assets/Scripts/ClickDemo.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+/// <summary>
+/// 点击事件接口
+/// </summary>
+public class ClickDemo : MonoBehaviour, IPointerClickHandler
+{
+    [SerializeField]
+    private Font font;  //字体
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        Debug.Log("点击事件");
+
+        Debug.Log(eventData.pointerEnter);
+
+        //1, 生成物体，设置名称为：ClickImageChild
+        GameObject imageChild = new GameObject("ClickImageChild");
+
+        //2, 设置该物体为ClickImage的子物体
+        imageChild.transform.SetParent(eventData.pointerEnter.transform);
+
+        //3, 为其动态添加Text组件
+        Text childText = imageChild.AddComponent<Text>();
+
+        //4, 设置Text组件文本内容
+        childText.text = "动态生成的Text组件";
+
+        //5, 设置Text组件字体
+        childText.font = font;
+
+        //6, 位置归零
+        imageChild.transform.localPosition = Vector3.zero;
+    }
+}
---
+++ EventInterface/Event/IDownAndIUpAndIClickDemo/Assets/Scripts/IDownAndUpDemo.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+/// <summary>
+/// 鼠标按下与抬起事件接口
+/// </summary>
+public class IDownAndUpDemo : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Debug.Log("鼠标按下");
+
+        Debug.Log(eventData.pointerEnter);
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        Debug.Log("鼠标抬起");
+
+        Debug.Log(eventData.pointerEnter);
+    }
+}
